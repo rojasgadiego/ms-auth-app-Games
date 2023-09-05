11f916ebc9e4d6401cddbb7de743bd40a40c6a7d
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-09-04 23:48:02 -0300

    gerAsignaturas - usuario

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -7,26 +7,28 @@ import { Usuario } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-
-  constructor (@InjectRepository(Usuario)
-  private readonly usuarioRepository: Repository<Usuario>) {}
-
+  constructor(
+    @InjectRepository(Usuario)
+    private readonly usuarioRepository: Repository<Usuario>,
+  ) {}
 
   create(createUsuarioDto: CreateUsuarioDto) {
     return 'This action adds a new usuario';
   }
 
   findAll() {
-    return this.usuarioRepository.find()
+    return this.usuarioRepository.find();
   }
 
   async findOne(email: string) {
     const usuario = await this.usuarioRepository.findOne({
-      where: { email }
+      where: { email },
     });
     if (!usuario) throw new NotFoundException('Asignatura not found');
     return usuario;
   }
+
+  getAsignaturas() {}
 
   update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
     return `This action updates a #${id} usuario`;
