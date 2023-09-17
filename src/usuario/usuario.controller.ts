baa0ef8743242326911b447d25b76849df87333b
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-09-16 22:19:03 -0300

    a

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -6,12 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('usuario')
+@UseGuards(JwtAuthGuard)
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
