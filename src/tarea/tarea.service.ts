f1568167e75429dec65d328187c312d8103e254a
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-10-05 00:21:28 -0300

    commons

--- src/tarea/tarea.service.ts
+++ src/tarea/tarea.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTareaDto } from './dto/create-tarea.dto';
 import { UpdateTareaDto } from './dto/update-tarea.dto';
 import { InjectRepository } from '@nestjs/typeorm';
